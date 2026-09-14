97bc303b05469bcdb562624dd7820620f95c75c9
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-14 19:33:37 +0300

    fix: port schema

--- src/schemas/port.schema.ts
+++ src/schemas/port.schema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 const MIN_PORT: number = 1;
 const MAX_PORT: number = 65535;
 
-export const PortSchema = z
+export const PortSchema = z.coerce
   .number({ error: "Порт має бути числом" })
   .int({ error: "Порт має бути цілим числом" })
   .min(MIN_PORT, { error: `Порт має бути не менше ніж ${MIN_PORT}` })
